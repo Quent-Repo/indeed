1cd857d4965eabe904c78881e5e331275b92efa0
Quent-Repo <grrkekisbest@gmail.com>
2024-07-07 14:05:05 -0400

    Working version

---
+++ Reviews.js
@@ -0,0 +1,8 @@
+##Show indeed reviews
+let g = document.getElementsByClassName("css-qodkr e1wnkr790").length;
+
+for(let i = 0;i<=g;i++){
+	document.getElementsByClassName("css-qodkr e1wnkr790")[0].ariaHidden="false"
+	document.getElementsByClassName("css-qodkr e1wnkr790")[0].className='css-1cxc9zk e1wnkr790'
+}
+
