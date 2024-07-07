4d90ef30fb56164a697efacbcc74f1ea4e40e3a0
Quent-Repo <grrkekisbest@gmail.com>
2024-07-07 14:06:47 -0400

    Added in somthing to show Pros and Cons

--- Reviews.js
+++ Reviews.js
@@ -6,3 +6,14 @@ for(let i = 0;i<=g;i++){
 	document.getElementsByClassName("css-qodkr e1wnkr790")[0].className='css-1cxc9zk e1wnkr790'
 }
 
+/* Not Working Yet
+##Pros and cons
+
+<div data-testid="reviewDescription" dir="auto" aria-hidden="true" class="css-hxk5yu eu4oa1w0">
+
+let b = document.getElementsByClassName("css-hxk5yu eu4oa1w0").length;
+for(let i = 0;i<=g;i++){
+	document.getElementsByClassName("css-hxk5yu eu4oa1w0")[0].ariaHidden="false"
+	document.getElementsByClassName("css-hxk5yueu4oa1w0")[0].color
+}
+*/
